9ee75abfc716f3e9ea0eb836956585938b515648
jjj960 <475921395hys@gmail.com>
2024-11-24 13:46:46 +0000

    编写makefile和dockerfile模版

---
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"goim/apps/user/rpc/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
