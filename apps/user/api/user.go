0d6e181004a92162c2d54e2889f6f722cc8e64be
jjj960 <475921395hys@gmail.com>
2024-11-25 13:58:37 +0000

    添加用户api的自定义响应

--- apps/user/api/user.go
+++ apps/user/api/user.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"goim/pkg/resultx"
 
 	"goim/apps/user/api/internal/config"
 	"goim/apps/user/api/internal/handler"
@@ -25,6 +27,9 @@ func main() {
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
+
+	httpx.SetErrorHandlerCtx(resultx.ErrHandler(c.Name))
+	httpx.SetOkHandler(resultx.OkHandler)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
