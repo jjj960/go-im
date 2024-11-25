5946ca08d88b0b47181359d1ad59b7ab6404790f
jjj960 <475921395hys@gmail.com>
2024-11-25 13:50:41 +0000

    添加用户api的注册和登录

---
+++ apps/user/api/internal/logic/user/detaillogic.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	"context"
+	"github.com/jinzhu/copier"
+	"goim/apps/user/rpc/user"
+	"goim/pkg/ctxdata"
+
+	"goim/apps/user/api/internal/svc"
+	"goim/apps/user/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DetailLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 获取用户信息
+func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogic {
+	return &DetailLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *DetailLogic) Detail(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
+	uid := ctxdata.GetUId(l.ctx)
+
+	userInfoResp, err := l.svcCtx.User.GetUserInfo(l.ctx, &user.GetUserInfoReq{
+		Id: uid,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var res types.User
+	copier.Copy(&res, userInfoResp.User)
+
+	return &types.UserInfoResp{Info: res}, nil
+}
--- apps/user/exec.sh
+++ apps/user/exec.sh
@@ -1,3 +1,5 @@
 #goctl rpc protoc ./rpc/user.proto --go_out=./rpc --go-grpc_out=./rpc --zrpc_out=./rpc    #grpc生成命令
 
-goctl model mysql ddl -src="../../deploy/sql/user.sql" -dir="./model" -c
+#goctl model mysql ddl -src="../../deploy/sql/user.sql" -dir="./model" -c
+
+goctl api go -api ./api/user.api -dir apps/user/api -style gozero
